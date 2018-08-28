925f49d33a54b4c0731886cb647a9523f962db4f
Fabrice Aneche <akh@nobugware.com>
2018-08-28 07:47:20 -0400

    open command

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -5,23 +5,36 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"runtime"
 	"strings"
 	"time"
 )
 
 var (
-	listen = flag.String("listen", ":8080", "listen address")
-	dir    = flag.String("dir", "htdocs", "directory to serve")
+	listen      = flag.String("listen", ":8080", "listen address")
+	dir         = flag.String("dir", "htdocs", "directory to serve")
+	openBrowser = flag.Bool("openBrowser", false, "open a browser while serving")
 )
 
 func main() {
 	flag.Parse()
 	log.Printf("listening on %s", *listen)
 
-	go func() {
-		time.Sleep(500 * time.Millisecond)
-		exec.Command("open", "http://127.0.0.1"+*listen).Run()
-	}()
+	cmd := ""
+	switch runtime.GOOS {
+	case "linux":
+		cmd = "xdg-open"
+	case "darwin":
+		cmd = "open"
+	case "windows":
+		cmd = "start"
+	}
+	if *openBrowser && cmd != "" {
+		go func() {
+			time.Sleep(500 * time.Millisecond)
+			exec.Command(cmd, "http://127.0.0.1"+*listen).Run()
+		}()
+	}
 	log.Fatal(http.ListenAndServe(*listen, http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
 		if strings.HasSuffix(req.URL.Path, ".wasm") {
 			resp.Header().Set("content-type", "application/wasm")
